e4e19771b20811e85d06d26fe9c4b5c135a4d07e
Krasutski <info@krasutski.com>
2017-06-28 16:41:05 +0300

    Added JNI to dynamic library

--- Dynamic Library/Windows/WUSB/WUSB/wusb.h
+++ Dynamic Library/Windows/WUSB/WUSB/wusb.h
@@ -1,23 +1,21 @@
 #ifndef WUSB_H
 #define WUSB_H
 
-#ifdef WUSB_Exports
-#define WUSB_API __declspec(dllexport)
+#ifdef _WINDOWS
+#define EXTERN_HEADER extern "C" __declspec(dllexport)
+#define WINAPI __stdcall
 #else
-#define WUSB_API __declspec(dllimport)
+#define EXTERN_HEADER extern "C"
 #endif
 
-extern "C"
- {
-  WUSB_API bool WINAPI AccessUSB(int DevNum);
-  WUSB_API bool WINAPI OpenUSB(int DevNum, DWORD baud);
-  WUSB_API bool WINAPI CloseUSB(void);
-  WUSB_API bool WINAPI PurgeUSB(void);
-  WUSB_API bool WINAPI RxFrameUSB(DWORD To, unsigned char &ADD,
-             unsigned char &CMD, unsigned char &N, unsigned char *Data);
-  WUSB_API bool WINAPI TxFrameUSB(unsigned char ADDR, unsigned char CMD,
-             unsigned char N, unsigned char *Data);
-  WUSB_API bool WINAPI NumUSB(DWORD &numDevs);
- }
+EXTERN_HEADER bool AccessUSB(int DevNum);
+EXTERN_HEADER bool OpenUSB(int DevNum, unsigned int baud);
+EXTERN_HEADER bool CloseUSB(void);
+EXTERN_HEADER bool PurgeUSB(void);
+EXTERN_HEADER bool RxFrameUSB(unsigned int To, unsigned char &ADD,
+	unsigned char &CMD, unsigned char &N, unsigned char *Data);
+EXTERN_HEADER bool TxFrameUSB(unsigned char ADDR, unsigned char CMD,
+	unsigned char N, unsigned char *Data);
+EXTERN_HEADER bool NumUSB(unsigned int &numDevs);
 
 #endif
